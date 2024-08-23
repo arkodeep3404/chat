d324769161e2a4b2ee39dc1caa2089642c9bab70
Arkodeep Chatterjee <arkodeep3404@gmail.com>
2024-08-24 03:13:04 +0530

    update

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+import { tool } from "@langchain/core/tools";
+
 import { DynamoDBChatMessageHistory } from "@langchain/community/stores/message/dynamodb";
 import { ChatOpenAI } from "@langchain/openai";
 import { RunnableWithMessageHistory } from "@langchain/core/runnables";
@@ -8,12 +10,8 @@ import {
   ChatPromptTemplate,
   MessagesPlaceholder,
 } from "@langchain/core/prompts";
-import { StringOutputParser } from "@langchain/core/output_parsers";
-
-const model = new ChatOpenAI({
-  modelName: "gpt-3.5-turbo",
-  openAIApiKey: process.env.OPENAI_API_KEY,
-});
+import { HumanMessage } from "@langchain/core/messages";
+import { AgentExecutor, createToolCallingAgent } from "langchain/agents";
 
 const prompt = ChatPromptTemplate.fromMessages([
   [
@@ -22,13 +20,39 @@ const prompt = ChatPromptTemplate.fromMessages([
   ],
   new MessagesPlaceholder("chat_history"),
   ["human", "{input}"],
+  ["placeholder", "{agent_scratchpad}"],
 ]);
 
-const chain = prompt.pipe(model).pipe(new StringOutputParser());
+const imageTool = tool(
+  async () => {
+    return "image url";
+  },
+  {
+    name: "Get-Image-Tool",
+    description:
+      "Use this tool if the user asks you to send them an image/picture",
+  }
+);
 
-const chainWithHistory = new RunnableWithMessageHistory({
-  runnable: chain,
+const llm = new ChatOpenAI({
+  modelName: "gpt-4o-mini",
+  openAIApiKey: process.env.OPENAI_API_KEY,
+});
+
+const tools = [imageTool];
+
+const agent = await createToolCallingAgent({
+  llm,
+  tools,
+  prompt,
+});
+
+const agentExecutor = new AgentExecutor({ agent, tools });
+
+const conversationalAgentExecutor = new RunnableWithMessageHistory({
+  runnable: agentExecutor,
   inputMessagesKey: "input",
+  outputMessagesKey: "output",
   historyMessagesKey: "chat_history",
   getMessageHistory: async (sessionId) => {
     return new DynamoDBChatMessageHistory({
@@ -58,8 +82,8 @@ const chainWithHistory = new RunnableWithMessageHistory({
   "Hello MJDeligan! It's nice to meet you. My name is AI. How may I assist you today?"
 */
 
-const res2 = await chainWithHistory.invoke(
-  { input: "What did I just say my name was?" },
+const res2 = await conversationalAgentExecutor.invoke(
+  { input: [new HumanMessage("send me a pic")] },
   { configurable: { sessionId: "test" } }
 );
 console.log(res2);
